4159a7b93841409457d92aeb69f3443c5461a554
kduferreira <ceduardoferreiraeng@gmail.com>
2023-11-03 17:33:34 -0300

    adicionando containers para o home page
    Signed-off-by: kduferreira <ceduardoferreiraeng@gmail.com>

---
+++ src/app/components/menu-tittle/menu-tittle.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-tittle',
+  templateUrl: './menu-tittle.component.html',
+  styleUrls: ['./menu-tittle.component.css']
+})
+export class MenuTittleComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+}
