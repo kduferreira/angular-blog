f210105da9cfecfdf6d2f5517faeebb7722a8b93
kduferreira <ceduardoferreiraeng@gmail.com>
2023-11-06 14:24:35 -0300

    content created
    Signed-off-by: kduferreira <ceduardoferreiraeng@gmail.com>

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":'1',
+    "title": 'Minha Noticia',
+    "description": 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate quidem magnam ea cumque repellat tempora, asperiores quia corrupti, fuga accusamus autem, aliquam hic. Voluptas deserunt quibusdam mollitia, quisquam sed facilis.',
+    "photoCouver": 'https://www.animeexplained.com/wp-content/uploads/2023/08/10-things-you-didnt-know-about-toji-fushiguro-jjk.webp'
+  },
+  {
+    "id":'2',
+    "title": "sai novo anime",
+    "description": "bla bla bla",
+    "photoCouver": 'https://t.ctcdn.com.br/iAOVsNADNCi_yq1JinkIS5slm_8=/1280x720/smart/i522181.png'
+  }
+
+]
